feff0b5d5e59571fe35c72e36dfd9d2b0500d5de
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-25 11:34:05 +0530

    minor update

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -61,6 +61,8 @@ const updateBlogController=asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,blog,"blog updated successfully"))
 })
 
+
+
 // for admin  with pagination
 
 
